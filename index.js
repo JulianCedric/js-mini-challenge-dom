11c383fa196a3ba8864da9f6bb7562982dc8a017
JulianCedric <julian.pormentilla@gmail.com>
2020-06-28 20:32:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-/***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
 
 
-/***** Deliverable 2 *****/
+// Weekend Practice (6/27-28):
 
 
-/***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+document.addEventListener("DOMContentLoader", () => {
+
+    const header = document.querySelector("h1#header")
+    console.log("Here's your header:", header)
+
+    header.style.color = "red"
+
+    console.log("PLAYERS array looks like this:", PLAYERS)
+
+        const players = document.getElementByClassName('player-container')
+
+        function createDiv () {
+            const createdDiv = document.createElement('div')
+            createdDiv.className = "player"
+            createdDiv.innerHTML =   
+        }
 
 
-/***** Deliverable 4 *****/
+
+    // players.appendChild() (??) 
+
+});
